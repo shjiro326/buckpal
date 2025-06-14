9ca65a06a4967e5cd7e99eb0894dc3f7c67182c7
DESKTOP-LO39MT7\shinj <iijimashinjiro@gmail.com>
2025-06-14 21:21:22 +0900

    pull request practice

--- src/main/java/io/reflectoring/buckpal/BuckPalApplication.java
+++ src/main/java/io/reflectoring/buckpal/BuckPalApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BuckPalApplication {
 
   public static void main(String[] args) {
-	String practiceBranch = "";
+	String practiceBranch = "practice";
     SpringApplication.run(BuckPalApplication.class, args);
   }
 
