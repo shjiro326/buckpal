17295364f6e77ac89bc73adb0514954787771ebc
DESKTOP-LO39MT7\shinj <iijimashinjiro@gmail.com>
2025-06-14 22:01:19 +0900

    push2

--- src/main/java/io/reflectoring/buckpal/BuckPalApplication.java
+++ src/main/java/io/reflectoring/buckpal/BuckPalApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BuckPalApplication {
 
   public static void main(String[] args) {
-	String practicePullRequest1 = "practicePullRequest1";
+	String practicePullRequest2 = "practicePullRequest2";
     SpringApplication.run(BuckPalApplication.class, args);
   }
 
